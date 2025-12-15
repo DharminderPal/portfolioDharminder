9a235fb8276f969ef6c7df9349e9ca31fcccd6cf
Dharminder Pal (CBT) <cbt.sd4@deepakfasteners.in>
2025-12-15 17:37:51 +0530

    15/12/2025

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useNavigate } from 'react-router'
 function App() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+const onclick =()=>{
+  useNavigate("/about");
+}
 
   return(
 <div>
